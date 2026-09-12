62cffc6aecda467d814fb9b178cae7eeb36c2049
musashimbt <musashimbt@gmail.com>
2026-09-11 21:34:03 -0300

    feat: implementa classe EntregadorMoto

--- SistemaLogistica/Models/EntregadorMoto.cs
+++ SistemaLogistica/Models/EntregadorMoto.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace SistemaLogistica.Models;
 
-namespace SistemaLogistica.Models
+public class EntregadorMoto : FuncionarioTransporte
 {
-    internal class EntregadorMoto
+    public string PlacaMoto { get; private set; }
+
+    public string RegiaoEntrega { get; private set; }
+
+
+    public EntregadorMoto(
+        string nome,
+        string registro,
+        string placaMoto,
+        string regiaoEntrega
+    ) : base(nome, registro)
     {
+        PlacaMoto = placaMoto;
+        RegiaoEntrega = regiaoEntrega;
+    }
+
+
+    public override void MostrarDetalhes()
+    {
+        base.MostrarDetalhes();
+
+        Console.WriteLine($"Placa da moto: {PlacaMoto}");
+        Console.WriteLine($"Região de entrega: {RegiaoEntrega}");
     }
 }
