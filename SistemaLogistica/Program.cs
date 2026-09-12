33c780c0bf19f6cd3a5b99c1c59ca7d599b680db
musashimbt <musashimbt@gmail.com>
2026-09-11 21:36:46 -0300

    feat: adiciona criacao e exibicao dos funcionarios

--- SistemaLogistica/Program.cs
+++ SistemaLogistica/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SistemaLogistica.Models;
+
+MotoristaCarreta motorista = new MotoristaCarreta(
+    "Carlos",
+    "FUNC001",
+    "ABC1D23",
+    "E"
+);
+
+EntregadorMoto entregador = new EntregadorMoto(
+    "Lucas",
+    "FUNC002",
+    "XYZ9A87",
+    "Zona Sul"
+);
+
+
+Console.WriteLine("=== MOTORISTA DE CARRETA ===");
+
+motorista.MostrarDetalhes();
+
+
+Console.WriteLine();
+
+
+Console.WriteLine("=== ENTREGADOR DE MOTO ===");
+
+entregador.MostrarDetalhes();
