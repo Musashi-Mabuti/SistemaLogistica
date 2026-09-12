ce266a7cf7ae3cb3d99f88cfd6e1acc81d9d14ae
musashimbt <musashimbt@gmail.com>
2026-09-11 21:31:55 -0300

    feat: implementa classe MotoristaCarreta

--- SistemaLogistica/Models/MotoristaCarreta.cs
+++ SistemaLogistica/Models/MotoristaCarreta.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace SistemaLogistica.Models;
 
-namespace SistemaLogistica.Models
+public class MotoristaCarreta : FuncionarioTransporte
 {
-    internal class MotoristaCarreta
+    public string PlacaCarreta { get; private set; }
+
+    public string CategoriaCnh { get; private set; }
+
+
+    public MotoristaCarreta(
+        string nome,
+        string registro,
+        string placaCarreta,
+        string categoriaCnh
+    ) : base(nome, registro)
     {
+        PlacaCarreta = placaCarreta;
+        CategoriaCnh = categoriaCnh;
+    }
+
+
+    public override void MostrarDetalhes()
+    {
+        base.MostrarDetalhes();
+
+        Console.WriteLine($"Placa da carreta: {PlacaCarreta}");
+        Console.WriteLine($"Categoria da CNH: {CategoriaCnh}");
     }
 }
